2c782c60e38514c6e522e23f933b14c0b80fe01c
Rion Sato <albus0sh@gmail.com>
2023-08-15 18:37:06 +0900

    フォームをfieldsetで囲んだ

--- app.py
+++ app.py
@@ -6,12 +6,9 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        gakumen = request.form['gakumen']
-        gakumen_int = int(gakumen)
-        kenkou_rate = request.form['kenkou_rate']
-        kenkou_rate_int = float(kenkou_rate)
-        kintouwari = request.form['kintouwari']
-        kintouwari_int = int(kintouwari)
-        return render_template("index.html", gakumen=gakumen, kenkou_rate=kenkou_rate, kintouwari=kintouwari) + tedori_calc.generate_tedori_result_str(gakumen_int, kenkou_rate_int, kintouwari_int)
+        gakumen = int(request.form['gakumen'])
+        kenkou_rate = float(request.form['kenkou_rate_ippan']) + float(request.form['kenkou_rate_kaigo'])
+        kintouwari = int(request.form['kintouwari'])
+        return render_template("index.html", gakumen=gakumen, kenkou_rate=kenkou_rate, kintouwari=kintouwari) + tedori_calc.generate_tedori_result_str(gakumen, kenkou_rate, kintouwari)
     else: # GET
         return render_template("index.html")
